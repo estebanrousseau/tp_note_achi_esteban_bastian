3e127cba30db385e4bee8abf6352e01ab19d930b
Rousseau Esteban <o22400319@or-iut-i207-f11.iut45.univ-orleans.fr>
2026-04-01 15:56:48 +0200

    sys question dans questionnaire.js

--- reponse/src/components/Questionnaire.vue
+++ reponse/src/components/Questionnaire.vue
@@ -2,7 +2,6 @@
 
 import Questions from './Questions.vue';
 
-
 export default{
     data() {
         return {
@@ -16,18 +15,49 @@ export default{
             reponse: '',
             score_total: 0,
             score_instant: 0,
-            score_visible: false
+            score_visible: false,
+            questionPoints: {}
         };
     },
     props: {
         questionnaire : Object
     },
+    computed: {
+        totalQuestions() {
+            return Array.isArray(this.questionnaire?.questions)
+                ? this.questionnaire.questions.length
+                : 0;
+        },
+        totalPossiblePoints() {
+            if (!Array.isArray(this.questionnaire?.questions)) {
+                return 0;
+            }
+            return this.questionnaire.questions.reduce((sum, question) => {
+                return sum + (Number(question.points) || 1);
+            }, 0);
+        }
+    },
     methods:{
         chgm_points : function($event) {
-            
+            if (!$event) {
+                return;
+            }
+
+            const question = $event.question ?? ($event[0] ?? null);
+            const points = $event.points ?? ($event[1] ?? $event.score ?? null);
+            if (!question || points === null || points === undefined) {
+                return;
+            }
+
+            const key = question.id ?? question.enonce ?? JSON.stringify(question);
+            const value = Number(points) || 0;
+            this.questionPoints[key] = value;
+            this.score_total = Object.values(this.questionPoints).reduce(
+                (sum, current) => sum + (Number(current) || 0),
+                0
+            );
         },
         valide_questionnaire : function() {
-            console.log(this.questionnaire)
             this.score_instant = this.score_total;
             this.score_visible = true;
         }
@@ -51,7 +81,7 @@ export default{
                 <!-- @remove="supprItem", @update="modifItem" -->
     </Questions>
     <div v-if="score_visible == true">
-        <p>Score : {{ score_instant }} / {{ questionnaire.questions }}</p>
+        <p>Score : {{ score_instant }} / {{ totalPossiblePoints }}</p>
     </div>
-    <button  type="button" @click="valide_questionnaire">Valider</button>
+    <button type="button" @click="valide_questionnaire">Valider</button>
 </template>
